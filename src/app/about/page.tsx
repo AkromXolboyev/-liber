a1f199dfd3ed47894dcc7f0aac292bc15efa2861
Akrom <akromxolboyev0615@gmail.com>
2024-08-31 10:14:39 +0500

    .

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
-const AboutLayout = ({children}:{children:React.ReactNode}) => {
+import React from 'react';
+import Link from 'next/link';
+
+const About = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='container w-1440px mx-auto'>
-        <div className='w-[1450px] p-5 gap-[20px] flex '>
-            <Link href={"/about"} className='p-5 bg-slate-200'>Аудиокитоб</Link>
-            <Link href={"/blog"} className='p-5 bg-slate-200'>Электрон китоблар</Link>
-            <Link href={"/news"} className='p-5 bg-slate-200'>Босма китоблар</Link>
-            <Link href={"/audio"} className='p-5 bg-slate-200'>Контакт</Link>
-            <Link href={"/contact"} className='p-5 bg-slate-200'>Биз хақимизда</Link>
-        </div>
-        <div>{children}</div>
+    <div className='container mx-auto max-w-screen-xl'>
+      <div className='w-full p-5 gap-5 flex flex-wrap bg-slate-100'>
+        <Link href="/about" className='p-5 bg-slate-200'>
+          Аудиокитоб
+        </Link>
+        <Link href="/blog" className='p-5 bg-slate-200'>
+          Электрон китоблар
+        </Link>
+        <Link href="/news" className='p-5 bg-slate-200'>
+          Босма китоблар
+        </Link>
+        <Link href="/audio" className='p-5 bg-slate-200'>
+          Контакт
+        </Link>
+        <Link href="/contact" className='p-5 bg-slate-200'>
+          Биз хақимизда
+        </Link>
+      </div>
+      <div>{children}</div>
     </div>
-  )
+  );
 }
 
-export default AboutLayout
+export default About;
