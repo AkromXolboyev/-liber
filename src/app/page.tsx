b7be43829c168c9b8d250a24d190480ed43b76be
Akrom <akromxolboyev0615@gmail.com>
2024-09-01 17:21:28 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,275 @@
+ import Image from "next/image";
+ import Link from "next/link";
+ export default function Home() {
+   return (
+      
+      <div>
+      <div className="w-[1400px] container p-5 gap-5 flex flex-wrap bg-slate-100">
+      <div>
+        <Link href="/about" className="p-5 bg-slate-100">
+          Аудиокитоб
+        </Link>
+      </div>
+      <div>
+        <Link href="/blog" className="p-5 bg-slate-100">
+          Электрон китоблар
+        </Link>
+      </div>
+      <div>
+        <Link href="/news" className="p-5 bg-slate-100">
+          Босма китоблар
+        </Link>
+      </div>
+      <div>
+        <Link href="/audio" className="p-5 bg-slate-100">
+          Контакт
+        </Link>
+      </div>
+      <div>
+        <Link href="/contact" className="p-5 bg-slate-100">
+          Биз хақимизда
+        </Link>
+      </div>
+    </div>
+    <div className="container mt-6 bg-blue-100 flex justify-between items-center">
+      <div className="flex justify-between">
+        <div>
+          <h1 className="pt-[30px]  font-bold text-[24px] ">
+            Кўп ўқилаётганлар
+          </h1>
+          <Image
+            src="/girl.svg"
+            alt=" girl"
+            className="dark:invert pt-[80px]"
+            width={200}
+            height={100}
+            priority
+          />
+        </div>
+        <div className="flex pl-[90px] gap-8">
+          <div className="m-auto">
+            <Image
+              src="/recent1.svg"
+              alt=" girl"
+              className="dark:invert "
+              width={200}
+              height={100}
+              priority
+            />
+            <h1 className="text-center">1984</h1>
+          </div>
+          <div className="m-auto">
+            <Image
+              src="/recent2.svg"
+              alt=" girl"
+              className="dark:invert "
+              width={200}
+              height={100}
+              priority
+            />
+            <h1 className="text-center">1984</h1>
+          </div>
+          <div className="m-auto">
+            <Image
+              src="/recent3.svg"
+              alt=" girl"
+              className="dark:invert "
+              width={200}
+              height={100}
+              priority
+            />
+            <h1 className="text-center">1984</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-[261px] h-[351px] bg-blue-600 items-center rounded-xl">
+        <h1 className="w-[199px] m-auto mt-10 text-center text-[24px] font-bold text-white">
+          Китоб ўқишни ёқтирасизми?
+        </h1>
+        <p className="w-[215px] m-auto text-center text-white pt-5">
+          Унда пулингизни тежаш учун ўзингиз йоқтирган рукнга обуна бўлинг
+        </p>
+        <button className="w-[210px] bg-white mt-[50px]   items-center flex gap-3 mx-auto justify-center h-[48px] font-bold rounded-xl">
+          <Image
+            src={"/love.svg"}
+            alt="icon"
+            className="dark:invert"
+            width={20}
+            height={20}
+            priority
+          />
+          Обуна бўлиш
+        </button>
+      </div>
+    </div>
+    <div className="container mt-14 flex justify-between">
+      <div className="w-[392px] flex bg-slate-200 gap-[16px]">
+        <Image
+          src={"/icon1.svg"}
+          alt="icon"
+          className="dark:invert ml-3"
+          width={48}
+          height={48}
+          priority
+        />
+        <div>
+          <h1 className="font-bold pb-2">Тезкор етказиш</h1>
+          <p className="text-[15px] font-light text-[#AAAAAA]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor
+          </p>
+        </div>
+      </div>
+      <div className="w-[392px] flex bg-slate-200 gap-[16px]">
+        <Image
+          src={"/icon1.svg"}
+          alt="icon"
+          className="dark:invert ml-3"
+          width={48}
+          height={48}
+          priority
+        />
+        <div>
+          <h1 className="font-bold pb-2">Тезкор етказиш</h1>
+          <p className="text-[15px] font-light text-[#AAAAAA]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor
+          </p>
+        </div>
+      </div>
+      <div className="w-[392px] flex bg-slate-200 gap-[16px]">
+        <Image
+          src={"/icon1.svg"}
+          alt="icon"
+          className="dark:invert ml-3"
+          width={48}
+          height={48}
+          priority
+        />
+        <div>
+          <h1 className="font-bold pb-2">Тезкор етказиш</h1>
+          <p className="text-[15px] font-light text-[#AAAAAA]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="container mt-[50px]">
+      <h1 className="text-[32px] font-semibold">Рукнлар</h1>
+      <div className="flex justify-between mt-[40px]">
+        <div>
+          <Image
+          
+            src={"/rukn1.svg"}
+            alt="icon"
+            className="dark:invert ml-3"
+            width={200}
+            height={164}
+            priority
+          />
+        
+        </div>
+        <div>
+          <Image
+            src={"/rukn6.svg"}
+            alt="icon"
+            className="dark:invert ml-3"
+            width={200}
+            height={164}
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={"/rukn3.svg"}
+            alt="icon"
+            className="dark:invert ml-3"
+            width={200}
+            height={164}
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={"/rukn4.svg"}
+            alt="icon"
+            className="dark:invert ml-3"
+            width={200}
+            height={164}
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={"/rukn5.svg"}
+            alt="icon"
+            className="dark:invert ml-3"
+            width={200}
+            height={164}
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            src={"/rukn2.svg"}
+            alt="icon"
+            className="dark:invert ml-3"
+            width={200}
+            height={164}
+            priority
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+   );
+ };
+{/* 
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-
 const Home = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <div className="w-full container p-5 gap-5 flex flex-wrap bg-slate-100">
+      <div className="w-[1400px] container p-5 gap-5 flex flex-wrap bg-slate-100">
         <div>
-          <Link href="/about" className="p-5 bg-slate-200">
+          <Link href="/about" className="p-5 bg-slate-100">
             Аудиокитоб
           </Link>
         </div>
         <div>
-          <Link href="/blog" className="p-5 bg-slate-200">
+          <Link href="/blog" className="p-5 bg-slate-100">
             Электрон китоблар
           </Link>
         </div>
         <div>
-          <Link href="/news" className="p-5 bg-slate-200">
+          <Link href="/news" className="p-5 bg-slate-100">
             Босма китоблар
           </Link>
         </div>
         <div>
-          <Link href="/audio" className="p-5 bg-slate-200">
+          <Link href="/audio" className="p-5 bg-slate-100">
             Контакт
           </Link>
         </div>
         <div>
-          <Link href="/contact" className="p-5 bg-slate-200">
+          <Link href="/contact" className="p-5 bg-slate-100">
             Биз хақимизда
           </Link>
         </div>
       </div>
-
       <div className="container mt-6 bg-blue-100 flex justify-between items-center">
         <div className="flex justify-between">
           <div>
-            <h1 className="pt-[30px] font-bold text-[24px]">Кўп ўқилаётганлар</h1>
+            <h1 className="pt-[30px]  font-bold text-[24px] ">
+              Кўп ўқилаётганлар
+            </h1>
             <Image
               src="/girl.svg"
-              alt="girl"
-              className="pt-[80px]"
+              alt=" girl"
+              className="dark:invert pt-[80px]"
               width={200}
               height={100}
               priority
@@ -50,7 +279,8 @@ const Home = ({ children }: { children: React.ReactNode }) => {
             <div className="m-auto">
               <Image
                 src="/recent1.svg"
-                alt="recent book 1"
+                alt=" girl"
+                className="dark:invert "
                 width={200}
                 height={100}
                 priority
@@ -60,7 +290,8 @@ const Home = ({ children }: { children: React.ReactNode }) => {
             <div className="m-auto">
               <Image
                 src="/recent2.svg"
-                alt="recent book 2"
+                alt=" girl"
+                className="dark:invert "
                 width={200}
                 height={100}
                 priority
@@ -70,7 +301,8 @@ const Home = ({ children }: { children: React.ReactNode }) => {
             <div className="m-auto">
               <Image
                 src="/recent3.svg"
-                alt="recent book 3"
+                alt=" girl"
+                className="dark:invert "
                 width={200}
                 height={100}
                 priority
@@ -87,10 +319,11 @@ const Home = ({ children }: { children: React.ReactNode }) => {
           <p className="w-[215px] m-auto text-center text-white pt-5">
             Унда пулингизни тежаш учун ўзингиз йоқтирган рукнга обуна бўлинг
           </p>
-          <button className="w-[210px] bg-white mt-[50px] items-center flex gap-3 mx-auto justify-center h-[48px] font-bold rounded-xl">
+          <button className="w-[210px] bg-white mt-[50px]   items-center flex gap-3 mx-auto justify-center h-[48px] font-bold rounded-xl">
             <Image
-              src="/love.svg"
+              src={"/love.svg"}
               alt="icon"
+              className="dark:invert"
               width={20}
               height={20}
               priority
@@ -99,13 +332,12 @@ const Home = ({ children }: { children: React.ReactNode }) => {
           </button>
         </div>
       </div>
-
       <div className="container mt-14 flex justify-between">
         <div className="w-[392px] flex bg-slate-200 gap-[16px]">
           <Image
-            src="/icon1.svg"
+            src={"/icon1.svg"}
             alt="icon"
-            className="ml-3"
+            className="dark:invert ml-3"
             width={48}
             height={48}
             priority
@@ -120,9 +352,9 @@ const Home = ({ children }: { children: React.ReactNode }) => {
         </div>
         <div className="w-[392px] flex bg-slate-200 gap-[16px]">
           <Image
-            src="/icon1.svg"
+            src={"/icon1.svg"}
             alt="icon"
-            className="ml-3"
+            className="dark:invert ml-3"
             width={48}
             height={48}
             priority
@@ -137,9 +369,9 @@ const Home = ({ children }: { children: React.ReactNode }) => {
         </div>
         <div className="w-[392px] flex bg-slate-200 gap-[16px]">
           <Image
-            src="/icon1.svg"
+            src={"/icon1.svg"}
             alt="icon"
-            className="ml-3"
+            className="dark:invert ml-3"
             width={48}
             height={48}
             priority
@@ -153,15 +385,26 @@ const Home = ({ children }: { children: React.ReactNode }) => {
           </div>
         </div>
       </div>
-
       <div className="container mt-[50px]">
         <h1 className="text-[32px] font-semibold">Рукнлар</h1>
         <div className="flex justify-between mt-[40px]">
           <div>
             <Image
-              src="/rukn1.svg"
+            
+              src={"/rukn1.svg"}
               alt="icon"
-              className="ml-3"
+              className="dark:invert ml-3"
+              width={200}
+              height={164}
+              priority
+            />
+          
+          </div>
+          <div>
+            <Image
+              src={"/rukn6.svg"}
+              alt="icon"
+              className="dark:invert ml-3"
               width={200}
               height={164}
               priority
@@ -169,9 +412,9 @@ const Home = ({ children }: { children: React.ReactNode }) => {
           </div>
           <div>
             <Image
-              src="/rukn6.svg"
+              src={"/rukn3.svg"}
               alt="icon"
-              className="ml-3"
+              className="dark:invert ml-3"
               width={200}
               height={164}
               priority
@@ -179,9 +422,9 @@ const Home = ({ children }: { children: React.ReactNode }) => {
           </div>
           <div>
             <Image
-              src="/rukn3.svg"
+              src={"/rukn4.svg"}
               alt="icon"
-              className="ml-3"
+              className="dark:invert ml-3"
               width={200}
               height={164}
               priority
@@ -189,9 +432,9 @@ const Home = ({ children }: { children: React.ReactNode }) => {
           </div>
           <div>
             <Image
-              src="/rukn4.svg"
+              src={"/rukn5.svg"}
               alt="icon"
-              className="ml-3"
+              className="dark:invert ml-3"
               width={200}
               height={164}
               priority
@@ -199,19 +442,9 @@ const Home = ({ children }: { children: React.ReactNode }) => {
           </div>
           <div>
             <Image
-              src="/rukn5.svg"
+              src={"/rukn2.svg"}
               alt="icon"
-              className="ml-3"
-              width={200}
-              height={164}
-              priority
-            />
-          </div>
-          <div>
-            <Image
-              src="/rukn2.svg"
-              alt="icon"
-              className="ml-3"
+              className="dark:invert ml-3"
               width={200}
               height={164}
               priority
@@ -223,4 +456,4 @@ const Home = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default Home;
+export default Home; */}
